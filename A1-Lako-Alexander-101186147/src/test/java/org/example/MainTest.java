03ed0f16df4be08eaa79210387236700e6d0da68
AlexanderLako <100615815+AlexanderLako@users.noreply.github.com>
2024-10-07 23:45:33 -0400

    R-TEST-01 Setup game decks and shuffle cards
    Added 4 tests for RESP 1. Test to check adventure deck size, check event deck size, adventure discard pile, and event discard pile.

--- A1-Lako-Alexander-101186147/src/test/java/org/example/MainTest.java
+++ A1-Lako-Alexander-101186147/src/test/java/org/example/MainTest.java
@@ -1,7 +1,52 @@
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+
+
+    @Test
+    @DisplayName("RESP 1.1: Test to check adventure deck size")
+    void RESP_1_test_1(){
+        Main game = new Main();
+        game.initializeAdventureDeck();
+
+        //Should be 100 cards in the adventure deck
+        int adventureDeckSize = game.getAdventureDeckSize();
+        assertEquals(100, adventureDeckSize);
+    }
+
+    @Test
+    @DisplayName("RESP 1.2: Test to check event deck size")
+    void RESP_1_test_2(){
+        Main game = new Main();
+        game.initializeEventDeck();
+
+        //Should be 17 cards in the event deck
+        int eventDeckSize = game.getEventDeckSize();
+        assertEquals(17, eventDeckSize);
+    }
+
+    @Test
+    @DisplayName("RESP 1.3: Test to check adventure discard pile is empty")
+    void RESP_1_test_3(){
+        Main game = new Main();
+
+        int adventureDiscardSize = game.getAdventureDiscardSize();
+        assertEquals(0, adventureDiscardSize);
+    }
+
+    @Test
+    @DisplayName("RESP 1.4 Test to check event discard pile is empty")
+    void RESP_1_test_4(){
+        Main game = new Main();
+
+        int eventDiscardSize = game.getEventDiscardSize();
+        assertEquals(0, eventDiscardSize);
+    }
+
 
 }
